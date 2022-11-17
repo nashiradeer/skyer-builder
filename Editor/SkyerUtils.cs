c022825ef11ab9c83952f808e322fe2be9ea98da
NashiraDeer <nashiradeer@deersoftware.dev>
2022-11-17 19:55:05 -0300

    Implement a method to converts Skyer sub-targets

--- Editor/SkyerUtils.cs
+++ Editor/SkyerUtils.cs
@@ -1,3 +1,6 @@
+using System;
+using UnityEditor;
+
 namespace DeerSoftware.SkyerBuilder
 {
     /// <summary>
@@ -94,6 +97,85 @@ namespace DeerSoftware.SkyerBuilder
                     {
                         SkyerSubtarget.None,
                     };
+            }
+        }
+
+        /// <summary>
+        /// Converts a Skyer sub-target and target to a sub-target int to be used in Unity Build Pipeline.
+        /// </summary>
+        /// <param name="target">Target used as a sub-target filter.</param>
+        /// <param name="subtarget">Sub-target to be converted.</param>
+        /// <returns>Sub-target compatible with the Unity Build Pipeline.</returns>
+        /// <exception cref="NotSupportedException">Throwed when a incompatible combination of <paramref name="target"/> and <paramref name="subtarget"/> is used.</exception>
+        public static int FromSkyerSubtarget(SkyerTarget target, SkyerSubtarget subtarget)
+        {
+            switch (subtarget)
+            {
+                case SkyerSubtarget.None:
+                    return 0;
+
+                case SkyerSubtarget.Server:
+                    if (target == SkyerTarget.Windows || target == SkyerTarget.Windows32 || target == SkyerTarget.Linux || target == SkyerTarget.Mac)
+                        return (int)StandaloneBuildSubtarget.Server;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                case SkyerSubtarget.DXT:
+                    if (target == SkyerTarget.WebGL)
+                        return (int)WebGLTextureSubtarget.DXT;
+                    else if (target == SkyerTarget.Android)
+                        return (int)MobileTextureSubtarget.DXT;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                case SkyerSubtarget.ETC2:
+                    if (target == SkyerTarget.WebGL)
+                        return (int)WebGLTextureSubtarget.ETC2;
+                    else if (target == SkyerTarget.Android)
+                        return (int)MobileTextureSubtarget.ETC2;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                case SkyerSubtarget.ASTC:
+                    if (target == SkyerTarget.WebGL)
+                        return (int)WebGLTextureSubtarget.ASTC;
+                    else if (target == SkyerTarget.Android)
+                        return (int)MobileTextureSubtarget.ASTC;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                case SkyerSubtarget.ETC:
+                    if (target == SkyerTarget.Android)
+                        return (int)MobileTextureSubtarget.ETC;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                case SkyerSubtarget.PVRTC:
+                    if (target == SkyerTarget.Android)
+                        return (int)MobileTextureSubtarget.PVRTC;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                case SkyerSubtarget.Master:
+                    if (target == SkyerTarget.XboxOne)
+                        return (int)XboxBuildSubtarget.Master;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                case SkyerSubtarget.Debug:
+                    if (target == SkyerTarget.XboxOne)
+                        return (int)XboxBuildSubtarget.Debug;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                case SkyerSubtarget.Package:
+                    if (target == SkyerTarget.PS4)
+                        return (int)PS4BuildSubtarget.Package;
+                    else
+                        throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
+
+                default:
+                    throw new NotSupportedException("Can't found a sub-target int for a target and/or subtarget");
             }
         }
     }
