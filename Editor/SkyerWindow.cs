6feaae2bd26b6af7310d7180b84af898d406c4a1
NashiraDeer <nashiradeer@deersoftware.dev>
2022-11-28 19:39:59 -0300

    Make Skyer Window works

--- Editor/SkyerWindow.cs
+++ Editor/SkyerWindow.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 namespace DeerSoftware.SkyerBuilder
@@ -11,8 +14,13 @@ namespace DeerSoftware.SkyerBuilder
         private string outputPath = "";
         private int target = 0;
         private int subtarget = 0;
+
         private bool configFoldout = true;
         private bool addTargetFoldout = true;
+        private bool targetsFoldout = true;
+
+        private Vector2 targetsSVPos = Vector2.zero;
+
         private List<SkyerPlatform> targets = new List<SkyerPlatform>();
 
         private void Awake()
@@ -28,16 +36,21 @@ namespace DeerSoftware.SkyerBuilder
 
         private void OnGUI()
         {
+            bool startBuild = false;
+
             if ((configFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(configFoldout, "Configuration")))
             {
                 EditorGUILayout.BeginHorizontal();
 
-                outputPath = EditorGUILayout.TextField("Output Path", "");
+                outputPath = EditorGUILayout.TextField("Output Path", outputPath);
 
                 if (GUILayout.Button("...", GUILayout.ExpandWidth(false)))
                     outputPath = EditorUtility.SaveFolderPanel("Choosing Output Path...", outputPath, PlayerSettings.productName);
 
                 EditorGUILayout.EndHorizontal();
+
+                if (GUILayout.Button("Start Build"))
+                    startBuild = true;
             }
 
             EditorGUILayout.EndFoldoutHeaderGroup();
@@ -73,19 +86,113 @@ namespace DeerSoftware.SkyerBuilder
                         targets.Add(platform);
                 }
             }
+
             EditorGUILayout.EndFoldoutHeaderGroup();
+
+            if ((targetsFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(targetsFoldout, "Targets")))
+            {
+                targetsSVPos = EditorGUILayout.BeginScrollView(targetsSVPos);
+
+                EditorGUILayout.BeginVertical();
+
+                for (int i = 0; i < targets.Count; i++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+
+                    SkyerPlatform wPlatform = targets[i];
+
+                    if (GUILayout.Button("X", GUILayout.MaxWidth(20)))
+                        targets.RemoveAt(i);
+
+                    string subtargetName = "";
+
+                    if (wPlatform.Subtarget != SkyerSubtarget.None)
+                        subtargetName = $" ({wPlatform.Subtarget})";
+
+                    EditorGUILayout.LabelField($"{wPlatform.Target}{subtargetName}");
+
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                EditorGUILayout.EndVertical();
+
+                EditorGUILayout.EndScrollView();
+            }
+
+            EditorGUILayout.EndFoldoutHeaderGroup();
+
+            if (startBuild)
+                StartBuild();
+
+            EditorUtility.ClearProgressBar();
         }
 
         private string[] GetExtraSubtargets(SkyerTarget target, SkyerSubtarget[] subtargets)
         {
+            string defaultSubtargetName = SkyerUtils.GetDefaultSubtargetName(target);
+
+            if (subtargets.Length <= 1)
+                return new string[] { defaultSubtargetName };
+
             List<string> subtargetNames = new List<string>();
 
-            subtargetNames.Add(SkyerUtils.GetDefaultSubtargetName(target));
+            subtargetNames.Add(defaultSubtargetName);
 
             for (int i = 1; i < subtargets.Length; i++)
                 subtargetNames.Add(Enum.GetName(typeof(SkyerSubtarget), subtargets[i]));
 
             return subtargetNames.ToArray();
+        }
+
+        private void StartBuild()
+        {
+            int waitTime = 1750;
+            int clocks = 144;
+            int errors = 0;
+
+            foreach (SkyerPlatform platform in targets)
+            {
+                string subtargetName = "";
+
+                if (platform.Subtarget != SkyerSubtarget.None)
+                    subtargetName = $"-{platform.Subtarget}";
+
+                string platformName = $"{platform.Target}{subtargetName}";
+
+                for (int i = 0; i < clocks; i++)
+                {
+                    if (EditorUtility.DisplayCancelableProgressBar("Skyer Builder", $"Preparing for building '{platformName}'...", (float)i / clocks))
+                    {
+                        EditorUtility.ClearProgressBar();
+                        return;
+                    }
+
+                    Thread.Sleep(waitTime / clocks);
+                }
+
+                string platformPath = Path.Combine(outputPath, platformName);
+
+                Directory.CreateDirectory(platformPath);
+
+                BuildPlayerOptions options = SkyerUtils.ToBuildPlayerOptions(platform, platformPath, PlayerSettings.productName);
+
+                List<string> scenes = new List<string>();
+
+                foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+                    if (scene.enabled) scenes.Add(scene.path);
+
+                options.scenes = scenes.ToArray();
+
+                if (BuildPipeline.BuildPlayer(options).summary.result != BuildResult.Succeeded)
+                    errors++;
+            }
+
+            for (int i = 0; i < clocks; i++)
+            {
+                EditorUtility.DisplayProgressBar("Skyer Builder", $"Compiled with {errors} possible errors...", (float)i / clocks);
+
+                Thread.Sleep(waitTime / clocks);
+            }
         }
     }
 }
