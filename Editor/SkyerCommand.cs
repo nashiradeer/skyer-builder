052ceecdb7a96f9e0de06ee29c00eaf8a474ea29
NashiraDeer <nashiradeer@deersoftware.dev>
2022-12-01 19:42:43 -0300

    Create a class for command line

---
+++ Editor/SkyerCommand.cs
@@ -0,0 +1,16 @@
+namespace DeerSoftware.SkyerBuilder
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class SkyerCommand
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Run()
+        {
+
+        }
+    }
+}
